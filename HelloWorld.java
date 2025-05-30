468c17644ad36e66eeae7292b36b2e96a75f403f
vcakey <hoangviethp2020@gmail.com>
2025-05-31 00:09:52 +0700

    lab3

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+// 1: HelloWorld.java
+//Text-printing program HelloWorld 
+
+public class HelloWorld {
+
+    public static void main(String args[]) {
+        System.out.println("Xin chao cac ban!");
+        System.out.println("Hello world!");
+    } // end of method main
+}
